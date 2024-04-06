80f7c855e03b611784ee5a6d2108d4824cb647ae
saketh.exe <psakethvishnu@gmail.com>
2024-04-06 22:09:19 +0530

    added login page

--- jobs/jobs.js
+++ jobs/jobs.js
@@ -3,24 +3,46 @@ let pop1 = document.getElementById("apply1");
 let pop2 = document.getElementById("apply2");
 let pop3 = document.getElementById("apply3");
 let pop = document.getElementsByClassName("Apply");
-setTimeout(()=>{loading.style.display = "none"} ,3500);
+let search = document.getElementById("search");
+let job_cards = document.getElementsByClassName("job-cards");
 
-function software_engi(){
+setTimeout(() => {
+    loading.style.display = "none";
+}, 3500);
+
+function software_engi() {
     pop1.style.display = "block";
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
 }
-function data_sci(){
+
+function data_sci() {
     pop2.style.display = "block";
 }
-function ai_engi(){
+
+function ai_engi() {
     pop3.style.display = "block";
 }
+
 function Close() {
     let pops = document.getElementsByClassName("Apply");
     for (let i = 0; i < pops.length; i++) {
         pops[i].style.display = "none";
     }
 }
+
+let jobTitles = document.querySelectorAll('.job-cards h2'); // Select all job title elements
+
+search.addEventListener("input", (e) => {
+    let search_term = e.target.value.trim().toLowerCase(); // Trim and convert search term to lowercase
+    for (let j = 0; j < job_cards.length; j++) {
+        let job_title = jobTitles[j].textContent.trim().toLowerCase(); // Get job title text content and convert to lowercase
+        if (job_title.includes(search_term)) {
+            job_cards[j].style.display = "block";
+        } else {
+            job_cards[j].style.display = "none";
+        }
+    }
+});
---
+++ login/login.js
@@ -0,0 +1,17 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 5000); // Change slide every 3 seconds
+}
+function redirectToIndex() {
+    // Use window.location.href for redirection
+    window.location.href = '../index.html';
+}

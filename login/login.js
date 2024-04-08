5cbabf44bf5b771ac50d04e4f831b581741a15e3
saketh.exe <psakethvishnu@gmail.com>
2024-04-08 14:46:47 +0530

    added signup page

--- login/login.js
+++ login/login.js
@@ -11,7 +11,27 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 5000); // Change slide every 3 seconds
 }
-function redirectToIndex() {
-    // Use window.location.href for redirection
-    window.location.href = '../index.html';
+// function redirectToIndex() {
+//     // Use window.location.href for redirection
+//     window.location.href = '../index.html';
+// }
+let log = document.getElementById("sign-up");
+let lc = document.getElementById("sign-co");
+function Login(){
+ 
+  log.style.animation = "Fade_Down 0.5s ease 1";
+  setTimeout(() =>{
+    lc.style.display = "none";
+  }, 100)
+  setTimeout(function() {
+      log.style.display = "none";
+  }, 300);
+}
+
+
+
+function signin(){
+  lc.style.display = "block";
+  log.style.display = "block";
+  log.style.animation = "signup 0.5s ease 1";
 }
